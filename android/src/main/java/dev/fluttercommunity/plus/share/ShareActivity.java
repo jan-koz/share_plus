2a5295d18dd3ac0a76163a8d92e1f53118cc5766
Jan Kozera <jan.kozera98@gmail.com>
2022-02-21 09:56:37 +0100

    add all imports to share activity

--- android/src/main/java/dev/fluttercommunity/plus/share/ShareActivity.java
+++ android/src/main/java/dev/fluttercommunity/plus/share/ShareActivity.java
@@ -1,3 +1,29 @@
+package dev.fluttercommunity.plus.share;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.BroadcastReceiver;
+import android.util.Log;
+import android.net.ConnectivityManager;
+import android.content.IntentFilter;
+import android.widget.Toast;
+
 public class ShareActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
